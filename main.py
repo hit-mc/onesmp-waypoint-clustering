3b825d7952cf54f4fa96957389cc27c82c590df7
Keuin <keuinx@gmail.com>
2021-09-25 21:37:09 +0800

    add GMM support.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # cluster algorithms
 from sklearn.cluster import KMeans
+from sklearn.mixture import GaussianMixture
 
 # configurations
 div8 = True
@@ -25,6 +26,13 @@ def cluster_by_k_means(data_, n_clusters: int):
     return km.labels_, km.cluster_centers_
 
 
+def cluster_by_gmm(data_, n_clusters: int):
+    data_ = np.array(data_)
+    gmm = GaussianMixture(n_components=n_clusters).fit(data_)
+    labels_ = gmm.predict(data_)
+    return labels_, gmm.means_
+
+
 def plot(data_: pd.DataFrame, labels_: list[int], centers_: pd.DataFrame, colors_: list[str], caption: str):
     # enforce datatype
     centers_ = pd.DataFrame(centers_)
@@ -43,11 +51,14 @@ def plot(data_: pd.DataFrame, labels_: list[int], centers_: pd.DataFrame, colors
 
 
 subplots = [331, 332, 333, 334, 335, 336, 337, 338, 339]
-plt.figure(figsize=(20.48, 15.36), dpi=100)
-for clusters, plt_id in zip(range(1, 10), subplots):
-    labels, centers = cluster_by_k_means(data, clusters)
-    plt.subplot(plt_id)
-    plot(data, labels, centers, colors, f'k-means, n_clusters={clusters}')
-    print('labels:', labels)
-    print('centers', centers)
-plt.show()
+cluster_methods = [cluster_by_k_means, cluster_by_gmm]
+for method in cluster_methods:
+    plt.figure(figsize=(20.48, 15.36), dpi=100)
+    plt.suptitle(method.__name__, fontsize=24, fontweight='bold')
+    for clusters, plt_id in zip(range(1, 10), subplots):
+        labels, centers = method(data, clusters)
+        plt.subplot(plt_id)
+        plot(data, labels, centers, colors, f'n_clusters={clusters}')
+        print('labels:', labels)
+        print('centers', centers)
+    plt.show()
