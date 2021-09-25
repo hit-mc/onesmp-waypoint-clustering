c2e1902541be7acb59fb57fdb83a22c2ae4ca397
Keuin <keuinx@gmail.com>
2021-09-25 22:07:45 +0800

    flexible rows and cols.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 
 # configurations
+ROWS, COLS = 3, 4
+MAX_CLUSTERS = ROWS * COLS
 div8 = True
 
 # load data
@@ -51,14 +53,14 @@ def plot(data_: pd.DataFrame, labels_: list[int], centers_, colors_: list[str], 
                 alpha=0.7)
 
 
-subplots = [331, 332, 333, 334, 335, 336, 337, 338, 339]
+subplots = [(ROWS, COLS, i) for i in range(1, MAX_CLUSTERS + 1)]
 cluster_methods = [cluster_by_k_means, cluster_by_gmm]
 for method in cluster_methods:
     plt.figure(figsize=(20.48, 15.36), dpi=100)
     plt.suptitle(method.__name__, fontsize=24, fontweight='bold')
-    for clusters, plt_id in zip(range(1, 10), subplots):
+    for clusters, plt_id in zip(range(1, MAX_CLUSTERS + 1), subplots):
         labels, centers = method(data, clusters)
-        plt.subplot(plt_id)
+        plt.subplot(*plt_id)
         plot(data, labels, centers, colors, f'n_clusters={clusters}')
         print('labels:', labels)
         print('centers', centers)
